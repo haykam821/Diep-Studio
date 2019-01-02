0ef0595fca6a47c8fcace1b23c1be22024199bb7
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-01-02 16:20:41 -0500

    Rewrite the sidebar in React

---
+++ sidebar.js
@@ -0,0 +1,278 @@
+const elem = React.createElement;
+
+class OptionsSection extends React.Component {
+	render() {
+		return elem("div", {
+			children: [
+				elem("h2", {
+					style: {
+						textAlign: "center",
+						marginBottom: 5,
+					},
+					children: this.props.header,
+				}),
+				elem("div", {
+					children: this.props.children,
+				}),
+			],
+		});
+	}
+}
+
+class ButtonPair extends React.Component {
+	render() {
+		return elem("div", {
+			style: {
+				display: "flex",
+			},
+			class: "buttonPair",
+			children: this.props.children,
+		});
+	}
+}
+
+class OptionRow extends React.Component {
+	render() {
+		const control = React.cloneElement(this.props.control, {
+			style: {
+				display: "inline-flex",
+				flexGrow: 100,
+				marginLeft: 8,
+			},
+		});
+
+		return elem("div", {
+			style: {
+				display: "flex",
+			},
+			children: [
+				elem("span", {
+					style: {
+						display: "inline-flex",
+					},
+				}, this.props.label + ":"),
+				control,
+			]
+		});
+	}
+}
+
+class Select extends React.Component {
+	render() {
+		return elem("select", {
+			...this.props,
+			children: this.props.options.map(option => {
+				return elem("option", {
+					children: option[0],
+					value: option[1],
+				});
+			}),
+		});
+	}
+}
+
+class Icon extends React.Component {
+	render() {
+		return elem("a", {
+			children: elem("i", {
+				class: "fab " + this.props.icon,
+			}),
+			title: this.props.title,
+			href: this.props.link,
+			target: "_blank",
+			style: {
+				fontSize: 25,
+				color: "white",
+				textDecoration: "none",
+				transition: "0.2s",
+				padding: 3,
+			}
+		});
+	}
+}
+
+ReactDOM.render(elem("div", {
+	children: [
+		elem("h1", {
+			style: {
+				textAlign: "center",
+				marginBottom: 5,
+			},
+			children: "Diep Studio"
+		}),
+		elem("p", null, "Welcome to Diep Studio! It is currently in beta; please send haykam any questions or concerns you may have. Thanks!"),
+		elem("div", {
+			id: "options",
+			style: {
+				margin: 12,
+			},
+			children: [
+				elem(OptionsSection, {
+					header: "Data Management",
+					children: [
+						elem("textarea", {
+							style: {
+								fontFamily: [
+									"Ubuntu Mono",
+									"monospace",
+								],
+							},
+							rows: 8,
+							placeholder: "Paste your scene here"
+						}),
+						elem(ButtonPair, {
+							children: [
+								elem("button", {
+									id: "import",
+									className: "dbtn-blue",
+									title: "Imports a Diep Studio or DSM scene.",
+									children: "Import",
+								}),
+								elem("button", {
+									id: "importSaved",
+									className: "dbtn-blue",
+									title: "his will replace the current scene with the one you last saved.",
+									children: "Import Saved",
+								}),
+							]
+						}),
+						elem(ButtonPair, {
+							children: [
+								elem("button", {
+									id: "export",
+									className: "dbtn-green",
+									title: "Saves and exports as JSON.",
+									children: "Save & Export",
+								}),
+							]
+						}),
+						elem(ButtonPair, {
+							children: [
+								elem("button", {
+									id: "reset",
+									className: "dbtn-red",
+									title: "This will remove all progress on the current scene!",
+									children: "Clear",
+								}),
+							]
+						}),
+					],
+				}),
+				elem(OptionsSection, {
+					header: "Camera",
+					children: [
+						elem(ButtonPair, {
+							children: [
+								elem("button", {
+									id: "jumpCenter",
+									class: "dbtn-red",
+								}, "Go to Center"),
+							]
+						}),
+						elem(ButtonPair, {
+							children: [
+								elem("button", {
+									id: "zoomOut",
+									class: "dbtn-blue",
+								}, "Zoom Out"),
+								elem("button", {
+									id: "zoomIn",
+									class: "dbtn-blue",
+								}, "Zoom In"),
+							]
+						}),
+					],
+				}),
+				elem(OptionsSection, {
+					header: "Toolbox",
+					children: [
+						elem("p", null, "You can use the digit keys to easily switch between the first 10 tools."),
+						elem("select", {
+							id: "toolSelect",
+						})
+					],
+				}),
+				elem(OptionsSection, {
+					header: "Grid Settings",
+					children: [
+						elem(OptionRow, {
+							label: "Grid Size",
+							control: elem("input", {
+								id: "config-gridSize",
+								type: "number",
+								min: 2,
+							}),
+						}),
+						elem(OptionRow, {
+							label: "Line Size",
+							control: elem("input", {
+								id: "config-gridLineWidth",
+								type: "number",
+								min: 1,
+							}),
+						}),
+						elem(OptionRow, {
+							label: "Grid Background",
+							control: elem("input", {
+								id: "config-backgroundColor",
+								type: "color",
+							}),
+						}),
+						elem(OptionRow, {
+							label: "Grid Color",
+							control: elem("input", {
+								id: "config-gridLineColor",
+								type: "color",
+							}),
+						}),
+					],
+				}),
+				elem(OptionsSection, {
+					header: "Border Settings",
+					children: [
+						elem(OptionRow, {
+							label: "Border Style",
+							control: elem(Select, {
+								id: "config-borderStyle",
+								options: [
+									["Diep.io", "newDiep"],
+									["Old Diep.io (single color)", "oldDiep"],
+									["Arras.io (this may be wrong)", "arras"],
+								],
+							}),
+						}),
+					],
+				}),
+			],
+		}),
+		elem("div", {
+			children: [
+				elem(Icon, {
+					title: "Reddit",
+					icon: "fa-reddit",
+					link: "https://www.reddit.com/r/DiepStudio/",
+				}),
+				elem(Icon, {
+					title: "GitHub",
+					icon: "fa-github",
+					link: "https://github.com/haykam821/Diep-Studio",
+				}),
+				elem(Icon, {
+					title: "Discord",
+					icon: "fa-discord",
+					link: "https://discord.gg/5yDRQdf",
+				}),
+			],
+			style: {
+				textAlign: "center",
+				position: "relative",
+				marginLeft: "auto",
+				marginRight: "auto",
+				left: 0,
+				right: 0,
+				bottom: 10,
+				paddingTop: 30,
+			},
+		}),
+	],
+}), document.getElementById("sidebar"));
