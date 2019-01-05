917f2907be149861b65e801b3f066c047081adbc
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-01-04 20:26:38 -0500

    Make global input styles constant

--- components/input.js
+++ components/input.js
@@ -10,17 +10,8 @@ const Input = configInput(class extends React.Component {
 			...this.props,
 			value: this.props.value || this.state.value,
 			style: {
-				backgroundColor: "#f8f8f8",
-				color: "black",
-				resize: "both",
-				lineHeight: 14,
-				font: "12px/16px Ubuntu, sans-serif",
-				fontWight: "normal",
-				border: "2px solid black",
-				borderRadius: 3,
-				userSelect: "text",
+				...formStyles,
 				padding: this.props.type === "color" ? 0 : 2,
-				outline: "none",
 				...this.props.style,
 			},
 		});
--- magic.js
+++ magic.js
@@ -8,4 +8,22 @@ function transitionVal(thing) {
 	}
 }
 
+const formStyles = {
+	backgroundColor: 0xf8f8f8,
+	color: 0x000000,
+	resize: "none",
+	lineHeight: "14px",
+	fontSize: 12,
+	fontFamily: [
+		"Ubuntu",
+		"sans-serif",
+	],
+	fontWeight: "normal",
+	border: "2px solid black",
+	borderRadius: 3,
+	userSelect: "text",
+	outline: "none",
+	margin: 0.5,
+};
+
 const configProps = {};
