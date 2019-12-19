c619491fbfa373f3a60575f98422998a9e24406d
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-12-18 21:44:56 -0500

    Use a plugin to generate the manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const ManifestPlugin = require("webpack-pwa-manifest");
+
 module.exports = {
 	entry: "./src/index.jsx",
 	mode: process.env.WEBPACK_MODE || "production",
@@ -13,4 +15,16 @@ module.exports = {
 		filename: "index.js",
 		path: path.join(__dirname, "/dist"),
 	},
+	plugins: [
+		new ManifestPlugin({
+			/* eslint-disable camelcase */
+			display: "standalone",
+			fingerprints: false,
+			inject: false,
+			lang: "en",
+			name: "Diep Studio",
+			short_name: "Studio",
+			/* eslint-enable camelcase */
+		}),
+	],
 };
